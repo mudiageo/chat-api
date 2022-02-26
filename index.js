220666c07090b0c54ad11800c8d81e9d2130e63e
mudiageo <76590594+mudiageo@users.noreply.github.com>
2022-02-26 07:54:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const axios = require("axios")
 const prompt = 'My name is Albert Einstein. I am a theoretical physicist who developed the theory of relativity. /n'
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const port = 3000;
 
-axios.post('http://api.vicgalle.net:5000/generate',{
+
+app.use(cors());
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.get('/', (req, res) => {
+    axios.post('http://api.vicgalle.net:5000/generate',{
     context: prompt,
 
                 temperature: 0.8,
@@ -21,8 +33,44 @@ axios.post('http://api.vicgalle.net:5000/generate',{
                 }
 })
   .then(function (response) {
-    console.log(response.data.text)
+    //console.log(response.data.text)
+res.send(response.data.text)
   })
   .catch(function (error) {
     console.log(error);
   });
+;
+});
+app.post('/', (req, res) => {
+   axios.post('http://api.vicgalle.net:5000/generate',{
+    context: prompt,
+
+                temperature: 0.8,
+
+                token_max_length: 100,
+
+                top_p: 1,
+
+                
+
+                stop_sequence: '\n',
+},
+  {
+                headers: {
+                    'Content-Type': 'application/json',
+                    
+                }
+})
+  .then(function (response) {
+    res.send(response.data.text)
+
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+});
+
+app.listen(port, () => console.log(`Chat API listening on port ${port}!`));
+
+    
